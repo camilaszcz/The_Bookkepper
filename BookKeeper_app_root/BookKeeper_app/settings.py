4c9598e413e7311a8375eee32eb344de4215e66d
camilaszcz <camila.szczerbacki@gmail.com>
2023-01-03 17:41:06 +0200

    Initial django app

--- BookKeeper_app_root/BookKeeper_app/settings.py
+++ BookKeeper_app_root/BookKeeper_app/settings.py
@@ -10,35 +10,40 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
+from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 import os
-import environ
+import sys
+import dj_database_url
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # # Initialise environment variables
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+# env = environ.Env(
+#     # set casting, default value
+#     DEBUG=(bool, False)
+# )
 
 # # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # False if not in os.environ because of casting above
-DEBUG = env('DEBUG')
+# DEBUG = env('DEBUG')
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
 
 
 # Raises Django's ImproperlyConfigured
 # exception if SECRET_KEY not in os.environ
 
-SECRET_KEY=env('SECRET_KEY')
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
@@ -57,6 +62,7 @@ INSTALLED_APPS = [
     "currently_reading",
     "accounts",
     "next_in_line",
+     "storages",
 ]
 
 MIDDLEWARE = [
@@ -69,6 +75,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+   
 ]
 
 ROOT_URLCONF = "BookKeeper_app.urls"
@@ -76,7 +83,8 @@ ROOT_URLCONF = "BookKeeper_app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        # "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -91,17 +99,33 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "BookKeeper_app.wsgi.application"
 
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':env('DATABASE_NAME'),
-        'USER':env('DATABASE_USER'),
-        'PASSWORD':env('DATABASE_PASSWORD'),
-        'HOST':env('DATABASE_HOST'),
-        'PORT':env('DATABASE_PORT'),
+
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
+    
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':env('DATABASE_NAME'),
+#         'USER':env('DATABASE_USER'),
+#         'PASSWORD':env('DATABASE_PASSWORD'),
+#         'HOST':env('DATABASE_HOST'),
+#         'PORT':env('DATABASE_PORT'),
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -125,7 +149,7 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Asia/Jerusalem"
 USE_I18N = True
 USE_TZ = True
-
+USE_L10N = True
 
 
 
@@ -134,13 +158,30 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
+
+
+# AWS_ACCESS_KEY_ID = 'key_id'
+# AWS_SECRET_ACCESS_KEY = 'access_key'
+# AWS_STORAGE_BUCKET_NAME = 'bucket_name'
+# AWS_DEFAULT_ACL = 'public-read' 
+# AWS_S3_ENDPOINT_URL = 'https://thebookkeeper.fra1.digitaloceanspaces.com' # Make sure nyc3 is correct
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400'
+# }
+
+# AWS_STATIC_LOCATION = 'static'
+# STATIC_URL = '%s/%s' % (AWS_S3_ENDPOINT_URL, AWS_STATIC_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'BookKeeper_app/static')
 ]
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# # Default primary key field type
+# # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
@@ -149,6 +190,8 @@ MEDIA_URL = '/media/'
 STATIC_DIRS = [
     os.path.join(BASE_DIR, 'BookKeeper_app/media')
 ]
+
+
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
